120ca605279c6556b96daacafc556d2bbc25e4dd
unknown <nicolaschaccal@gmail.com>
2020-08-21 17:08:08 -0300

    prueba 2

--- js/init.js
+++ js/init.js
@@ -46,41 +46,13 @@ var auth2;
 var googleUser; // The current user
 
 function init() {
-  gapi.load('auth2', function(){
+  gapi.load('auth2', () => {
     auth2 = gapi.auth2.init({
-        client_id: '879171387926-h5b1okbkhir4hd4p7bh4ch8mmm7pdl5a.apps.googleusercontent.com'
+      client_id: '879171387926-h5b1okbkhir4hd4p7bh4ch8mmm7pdl5a.apps.googleusercontent.com',
+      fetch_basic_profile: true,
+      ux_mode: redirect,
+      redirect_uri: ''
     });
-    auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
-
-    auth2.isSignedIn.listen(signinChanged);
-    auth2.currentUser.listen(userChanged); // This is what you use to listen for user changes
-});  
-
-var signinChanged = function (val) {
-    console.log('Signin state changed to ', val);
-};
-
-var onSuccess = function(user) {
-    console.log('Signed in as ' + user.getBasicProfile().getName());
-    // Redirect somewhere
-};
-
-var onFailure = function(error) {
-    console.log(error);
-};
-
-function signOut() {
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}        
-
-var userChanged = function (user) {
-    if(user.getId()){
-      // Do something here
-    }
-};
-}
 
 
 
