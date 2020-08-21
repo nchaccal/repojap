f66356a73df8de6b70fc3c867eebb5b448f1dc50
unknown <nicolaschaccal@gmail.com>
2020-08-21 16:01:43 -0300

    ff

--- js/init.js
+++ js/init.js
@@ -82,17 +82,7 @@ var userChanged = function (user) {
 };
 }
 
-// auth2 is initialized with gapi.auth2.init() and a user is signed in.
 
-if (auth2.isSignedIn.get()) {
-  var profile = auth2.currentUser.get().getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
