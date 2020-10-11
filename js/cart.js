876311e5a10d050b9ece9b67df3899dc3275e42a
Nicolás Chaccal <nicolaschaccal@gmail.com>
2020-10-11 20:25:28 -0300

    changes in cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,149 @@
+var cartInfo = {};
+let subtotalFinal = 0;
+
+
+
+//función para construir el carrito de compra con los elementos del json
+function showArticles () {
+
+            let htmlContentToAppend = "";
+
+            for(let i = 0; i < cartInfo.articles.length; i++){
+                let artcilesInfo = cartInfo.articles[i];
+                let subtotal = (artcilesInfo.count * artcilesInfo.unitCost);
+
+                htmlContentToAppend += 
+                `
+            <div class="container">
+                <div class="row">
+                        
+                    <div class="col">
+                        <img alt="" src="` + artcilesInfo.src + `" width="80" height="80">
+                    </div>
+
+                    <div class="col">
+                        <p class="mb-1">` + artcilesInfo.name + `</p>
+                    </div>
+
+                    <div class="col">
+                        <span>` + artcilesInfo.currency + `</span>
+                        <span class="productpriceUnit">` + artcilesInfo.unitCost + `</span>
+                    </div>
+
+                    <div class="col">
+                        <input type="number" " style="width:35%" value="` + artcilesInfo.count + `">
+                    </div>
+
+                    <div class="col">
+                        <span class="currency">` + artcilesInfo.currency + `</span>
+                        <span class="productSubTotal">` + subtotal + `</span>
+                    </div>
+
+                </div>
+          
+            </div>
+            <hr class="my-3">
+                `
+        
+                document.getElementById("cartContent").innerHTML = htmlContentToAppend;
+                
+    }
+}
+
+//esta función calcula el subtotal individual de cada artículo
+function subtotalIndividual () {
+
+    //aplico un for para recorrer todos los elementos del json. No utilizo ID para obtener los elementos. Uso otros selectores y coloco [i] para llamar a la cantidad específica. 
+    
+    for(let i = 0; i < cartInfo.articles.length; i++){
+        document.getElementsByTagName("input")[i].addEventListener("change", function(){
+            //calcula el valor del subtotal individual
+            document.querySelectorAll(".productSubTotal")[i].innerHTML = (document.querySelectorAll(".productpriceUnit")[i].innerHTML * document.getElementsByTagName("input")[i].value);
+
+            //Cambio el precio del subtotal individual con cada iteracción y el precio del subtotal global 
+            let subtotalFinal = 0;
+            for(let b= 0; b < cartInfo.articles.length; b++){
+                individualPriceProduct = document.querySelectorAll(".productSubTotal")[b].innerHTML;
+                //condición que establece que si el precio está en dólares, lo cambie a pesos en el subtotal
+                if (document.querySelectorAll(".currency")[b].innerHTML == "USD") {
+                    subtotalFinal += (individualPriceProduct * 40);
+                } else {
+                    subtotalFinal += (individualPriceProduct * 1);
+                }
+                //Agarra el elemento subtotal del HTML y le pone el valor dinámico del precio
+                   document.getElementById("subtotalGlobal").innerHTML = "UYU" + " " + subtotalFinal;
+                   document.getElementById("totalGlobal").innerHTML = "UYU" + " " + subtotalFinal;
+                }
+        });
+                        
+    }
+}
+
+//función para mostrar el subtotal cuando la página se carga por primera vez. La repito para poder mostrar el total cuando cargue la página por primera vez nada más.
+function subtotalGlobal (){ 
+    let subtotalFinal = 0;
+    for(let b= 0; b < cartInfo.articles.length; b++){
+    individualPriceProduct = document.querySelectorAll(".productSubTotal")[b].innerHTML;
+    
+    if (document.querySelectorAll(".currency")[b].innerHTML == "USD") {
+        subtotalFinal += (individualPriceProduct * 40);
+    } else {
+        subtotalFinal += (individualPriceProduct * 1);
+    }
+       document.getElementById("subtotalGlobal").innerHTML = "UYU" + " " + subtotalFinal;
+       document.getElementById("totalGlobal").innerHTML = "UYU" + " " + subtotalFinal;
+       
+    }
+}
+
+function shippingPrice () {  
+    //recorro cada opción de envío y le asigno un evento change a cada uno de ellos. Coloco 3 en lugar de lenght porque ya sé la cantidad de opciones de tipo input. 
+    for(let i = 0; i < 3; i++){
+        document.getElementsByName("carrito")[i].addEventListener("change", function(){
+        
+            if (document.getElementsByName("carrito")[i] == document.getElementsByName("carrito")[0]) {
+                document.getElementById("shippingCost").innerHTML = 0.15;
+                
+            } 
+            
+            else if (document.getElementsByName("carrito")[i] == document.getElementsByName("carrito")[1]) {
+                document.getElementById("shippingCost").innerHTML = 0.07;
+            } 
+            
+            else {
+                document.getElementById("shippingCost").innerHTML = 0.05;
+            }
+        });
+        
+    }   
+}
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            cartInfo = resultObj.data;
+
+            showArticles (cartInfo);
+            subtotalIndividual (cartInfo);
+            subtotalGlobal (cartInfo);
+            shippingPrice (cartInfo);
+
+
+
+}
+
+
+});
+
+
 
 });
